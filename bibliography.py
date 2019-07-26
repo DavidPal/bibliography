c6b3ca8fd232b6f7896f1a9b16187f3313d624ea
David Pal <davidko.pal@gmail.com>
2019-07-25 21:55:07 -0400

    Modernize code and remove extra empty line at the end of bibfile

--- bibliography.py
+++ bibliography.py
@@ -3,7 +3,7 @@
 # BibTeX bibliography beautifier.
 #
 # Author: David Pal <davidko.pal@gmail.com>
-# Date: 2013, 2014, 2015
+# Date: 2013-2019
 #
 # Usage:
 #
@@ -15,247 +15,257 @@
 #   bibliography.py input.bib > output.bib
 
 import re
-import string
 import sys
 
-entry_types= {
-               'article'       : 'Article',
-               'book'          : 'Book',
-               'booklet'       : 'Booklet',
-               # 'Conference' is the same as 'InProceedings'
-               'conference'    : 'InProceedings',
-               'inbook'        : 'InBook',
-               'incollection'  : 'InCollection',
-               'inproceedings' : 'InProceedings',
-               'manual'        : 'Manual',
-               'mastersthesis' : 'MastersThesis',
-               'misc'          : 'Misc',
-               'phdthesis'     : 'PhDThesis',
-               'proceedings'   : 'Proceedings',
-               'techreport'    : 'TechReport',
-               'unpublished'   : 'Unpublished',
-               'string'        : 'String',
-             }
+entry_types = {
+    'article': 'Article',
+    'book': 'Book',
+    'booklet': 'Booklet',
+    'conference': 'InProceedings',  # 'Conference' is the same as 'InProceedings'
+    'inbook': 'InBook',
+    'incollection': 'InCollection',
+    'inproceedings': 'InProceedings',
+    'manual': 'Manual',
+    'mastersthesis': 'MastersThesis',
+    'misc': 'Misc',
+    'phdthesis': 'PhDThesis',
+    'proceedings': 'Proceedings',
+    'techreport': 'TechReport',
+    'unpublished': 'Unpublished',
+    'string': 'String',
+}
 
 months = {
-                'jan'  :  'January',
-                'feb'  :  'Februry',
-                'mar'  :  'March',
-                'apr'  :  'April',
-                'may'  :  'May',
-                'jun'  :  'June',
-                'jul'  :  'July',
-                'aug'  :  'August',
-                'sep'  :  'September',
-                'oct'  :  'October',
-                'nov'  :  'November',
-                'dec'  :  'December',
-         }
+    'jan': 'January',
+    'feb': 'Februry',
+    'mar': 'March',
+    'apr': 'April',
+    'may': 'May',
+    'jun': 'June',
+    'jul': 'July',
+    'aug': 'August',
+    'sep': 'September',
+    'oct': 'October',
+    'nov': 'November',
+    'dec': 'December',
+}
 
-def Format(text):
-	return ' '.join(text.split())
 
-def Capitalize(text):
-	word_start = True
-	s = ''
-	for c in text.lower():
-		if word_start:
-			c = c.upper()
-		word_start = not c.isalpha()
-		s = s + c
-	return s
+def format_text(text):
+    return ' '.join(text.split())
 
-def FindMatchingParenthesis(text):
-	nesting = 1
-	end = 0
-	for i in range(1, len(text)):
-		if text[i] == '{':
-			nesting = nesting + 1
-		elif text[i] == '}':
-			nesting = nesting - 1
-		end = i
-		if nesting == 0:
-			break
-	end = end + 1
-	return (text[:end], text[end:])
 
-def RemoveBraces(text):
-	if text[0] == '{':
-		 text = text[1:]
-	if text[-1] == '}':
-		 text = text[:-1]
-	return text
+def capitalize(text):
+    word_start = True
+    s = ''
+    for c in text.lower():
+        if word_start:
+            c = c.upper()
+        word_start = not c.isalpha()
+        s = s + c
+    return s
 
-def NormalizeAuthor(text):
-	parts = text.split(',', 1)
-	if len(parts) >= 2:
-		return parts[1].strip() + ' ' + parts[0].strip()
-	return parts[0].strip()
 
-def NormalizeAuthors(text):
-	authors = text.split(' and ')
-	return ' and '.join([NormalizeAuthor(author) for author in authors])
+def find_matching_parenthesis(text):
+    nesting = 1
+    end = 0
+    for i in range(1, len(text)):
+        if text[i] == '{':
+            nesting = nesting + 1
+        elif text[i] == '}':
+            nesting = nesting - 1
+        end = i
+        if nesting == 0:
+            break
+    end = end + 1
+    return text[:end], text[end:]
 
-def NormalizePages(text):
-	parts = text.split('--', 1)
-	if not '--' in text:
-		parts = text.split('-', 1)
-	normalized = parts[0].strip()
-	if len(parts) >= 2:
-		normalized = parts[0].strip() + '--' + parts[1].strip()
-	return normalized
 
-def SafeParseInt(text):
-	try:
-        	return int(text)
-	except ValueError:
-		return None
+def remove_braces(text):
+    if text[0] == '{':
+        text = text[1:]
+    if text[-1] == '}':
+        text = text[:-1]
+    return text
 
-def NormalizeYear(text):
-	year = SafeParseInt(text)
-	if not year:
-		return text.strip()
-	if (year >= 10) and (year <= 99):
-		return str(1900 + year)
-	return str(year)
 
-def NormalizeMonth(text):
-	prefix = text[:3].lower()
-	if prefix in months:
-		return months[prefix]
-	return text
+def normalize_author(text):
+    parts = text.split(',', 1)
+    if len(parts) >= 2:
+        return format_text(parts[1]) + ' ' + format_text(parts[0].strip())
+    return format_text(parts[0])
+
+
+def normalize_authors(text):
+    authors = text.split(' and ')
+    return ' and '.join([normalize_author(author) for author in authors])
+
+
+def normalize_pages(text):
+    parts = text.split('--', 1)
+    if '--' not in text:
+        parts = text.split('-', 1)
+    normalized = parts[0].strip()
+    if len(parts) >= 2:
+        normalized = parts[0].strip() + '--' + parts[1].strip()
+    return normalized
+
+
+def safe_parse_int(text):
+    try:
+        return int(text)
+    except ValueError:
+        return None
+
+
+def normalize_year(text):
+    year = safe_parse_int(text)
+    if not year:
+        return text.strip()
+    if (year >= 10) and (year <= 99):
+        return str(1900 + year)
+    return str(year)
+
+
+def normalize_month(text):
+    prefix = text[:3].lower()
+    if prefix in months:
+        return months[prefix]
+    return text
 
 
 # An entry object
 class Entry(object):
-	def __init__(self):
-		self.entry_type = 'UNKNOWN'
-		self.entry_name = ''
-		self.rows = { }
+    def __init__(self):
+        self.entry_type = 'UNKNOWN'
+        self.entry_name = ''
+        self.rows = {}
 
-	def ParseFromString(self, text):
-		m = re.match('\s*@\s*(\w+)\s*({)\s*', text)
-		if not m:
-			return None
-		self.entry_type = m.group(1)
-		self.entry_type = self.NormalizedEntryType()
-		text = text[m.end(2):]
-		text, rest = FindMatchingParenthesis(text)
+    def parse_from_string(self, text):
+        m = re.match('\\s*@\\s*(\\w+)\\s*({)\\s*', text)
+        if not m:
+            return None
+        self.entry_type = m.group(1)
+        self.entry_type = self.normalized_entry_type()
+        text = text[m.end(2):]
+        text, rest = find_matching_parenthesis(text)
 
-		m = re.match('\s*([^\s]+)\s*,\s*', text)
-		if m:
-			self.entry_name = m.group(1)
-			text = text[m.end():]
+        m = re.match('\\s*([^\\s]+)\\s*,\\s*', text)
+        if m:
+            self.entry_name = m.group(1)
+            text = text[m.end():]
 
-		while text:
-			text = self.ParseRow(text)
-		return rest
+        while text:
+            text = self.parse_row(text)
+        return rest
 
-	def ParseRow(self, text):
-		m = re.match('\s*,?\s*([\w-]+)\s*=\s*', text)
-		if not m:
-			return None
-		key = m.group(1)
-		if not self.entry_type == 'String':
-			key = Capitalize(key)
-		text = text[m.end():]
+    def parse_row(self, text):
+        m = re.match('\\s*,?\\s*([\\w-]+)\\s*=\\s*', text)
+        if not m:
+            return None
+        key = m.group(1)
+        if not self.entry_type == 'String':
+            key = capitalize(key)
+        text = text[m.end():]
 
-		value = ''
-		if text[0] == '{':
-			value, rest = FindMatchingParenthesis(text)
-			value = RemoveBraces(value)
-		elif text[0] == '\"':
-			m = re.match('^"([^\"]+)"\s*,?\s*', text)
-			value = m.group(1)
-			rest = text[m.end():]
-		else:
-			m = re.match('\s*(\w+)\s*,?\s*', text)
-			value = m.group(1)
-			rest = text[m.end():]
+        value = ''
+        if text[0] == '{':
+            value, rest = find_matching_parenthesis(text)
+            value = remove_braces(value)
+        elif text[0] == '\"':
+            m = re.match('^"([^\"]+)"\\s*,?\\s*', text)
+            value = m.group(1)
+            rest = text[m.end():]
+        else:
+            m = re.match('\\s*(\\w+)\\s*,?\\s*', text)
+            value = m.group(1)
+            rest = text[m.end():]
 
-		self.rows[key] = value.strip()
-		return rest
+        self.rows[key] = value.strip()
+        return rest
 
-	def NormalizedEntryType(self):
-		entry_type = self.entry_type.lower()
-		if entry_type in entry_types:
-			entry_type = entry_types[entry_type]
-		return entry_type
+    def normalized_entry_type(self):
+        entry_type = self.entry_type.lower()
+        if entry_type in entry_types:
+            entry_type = entry_types[entry_type]
+        return entry_type
 
-	def ToString(self):
-		s = '@' + self.entry_type + '{'
-		if self.entry_name:
-			s += self.entry_name + ','
-		s += '\n'
-		keys = sorted(self.rows.keys())
-		for key in keys:
-			s += + 4*' '
-			s += key
-			s += max(0, 13-len(key))*' '
-			s += ' = '
-			value = self.rows[key]
-			if (not self.entry_type == 'String'):
-				if (key in ['Author', 'Editor']):
-					value = NormalizeAuthors(value)
-				if (key == 'Pages'):
-					value = NormalizePages(value)
-				if (key == 'Year'):
-					value = NormalizeYear(value)
-				if (key == 'Month'):
-					value = NormalizeMonth(value)
+    def __str__(self):
+        s = '@' + self.entry_type + '{'
+        if self.entry_name:
+            s += self.entry_name + ','
+        s += '\n'
+        keys = sorted(self.rows.keys())
+        for key in keys:
+            s += + 4 * ' '
+            s += key
+            s += max(0, 13 - len(key)) * ' '
+            s += ' = '
+            value = self.rows[key]
+            if not self.entry_type == 'String':
+                if key in ['Author', 'Editor']:
+                    value = normalize_authors(value)
+                if key == 'Pages':
+                    value = normalize_pages(value)
+                if key == 'Year':
+                    value = normalize_year(value)
+                if key == 'Month':
+                    value = normalize_month(value)
 
-			s += '{' + Format(value) + '}'
-			if self.entry_type != 'String':
-				s += ','
-			s += '\n'
-		return s + '}\n'
+            s += '{' + format_text(value) + '}'
+            if self.entry_type != 'String':
+                s += ','
+            s += '\n'
+        return s + '}'
 
-	def SortKey(self):
-		priorities = {
-               		'String'        : -99,
-               		'Proceedings'   : 99,
-               		'Book'          : 99,
-	      	}
-		if self.entry_type in priorities:
-			return priorities[self.entry_type]
-		return 0
-
-
-def ParseEntries(text):
-	entries = []
-	while True:
-		e = Entry()
-		text = e.ParseFromString(text)
-		if not text:
-			break
-		entries.append(e)
-	return entries
-
-def SortEntries(entries):
-	entries.sort(key=lambda e: e.entry_name)
-	entries.sort(key=lambda e: e.entry_type)
-	entries.sort(key=lambda e: e.SortKey())
-	return entries
+    def sort_key(self):
+        priorities = {
+            'String': -99,
+            'Proceedings': 99,
+            'Book': 99,
+        }
+        if self.entry_type in priorities:
+            return priorities[self.entry_type]
+        return 0
 
 
-def ReadFile():
-	lines = []
-	with open(sys.argv[1], 'r') as f:
-		for line in f:
-			if line.strip().startswith('%'):
-				print(line.strip())
-			else:
-				lines.append(line)
-	text = '\n'.join(lines)
-	return text
+def parse_entries(text):
+    entries = []
+    while True:
+        e = Entry()
+        text = e.parse_from_string(text)
+        if not text:
+            break
+        entries.append(e)
+    return entries
+
+
+def sort_entries(entries):
+    entries.sort(key=lambda e: e.entry_name)
+    entries.sort(key=lambda e: e.entry_type)
+    entries.sort(key=lambda e: e.sort_key())
+    return entries
+
+
+def read_file():
+    lines = []
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            if line.strip().startswith('%'):
+                print(line.strip())
+            else:
+                lines.append(line)
+    text = '\n'.join(lines)
+    return text
+
 
 # main
 def main():
-	text = ReadFile()
-	entries = ParseEntries(text)
-	entries = SortEntries(entries)
-	for entry in entries:
-		print(entry.ToString())
+    text = read_file()
+    entries = parse_entries(text)
+    entries = sort_entries(entries)
+    print("\n\n".join(str(entry) for entry in entries))
+
 
 if __name__ == '__main__':
     main()
--- test_bibliography.py
+++ test_bibliography.py
@@ -3,50 +3,54 @@
 import bibliography
 import unittest
 
+
 class TestBibliography(unittest.TestCase):
 
-	def test_Format(self):
-		self.assertEqual('hello world', bibliography.Format(' hello   \n world \r \t '))
+	def test_format_text(self):
+		self.assertEqual('hello world', bibliography.format_text(' hello   \n world \r \t '))
 
-	def test_Capitalize(self):
-		self.assertEqual('Hello World', bibliography.Capitalize('hello world'))
+	def test_capitalize(self):
+		self.assertEqual('Hello World', bibliography.capitalize('hello world'))
 
-	def test_RemoveBraces(self):
-		self.assertEqual('hello world', bibliography.RemoveBraces('{hello world}'))
+	def test_remove_braces(self):
+		self.assertEqual('hello world', bibliography.remove_braces('{hello world}'))
 
-	def test_FindMatchingParenthesis(self):
-		result1, result2 = bibliography.FindMatchingParenthesis('{hello {world} 123} END }')
+	def test_find_matching_parenthesis(self):
+		result1, result2 = bibliography.find_matching_parenthesis('{hello {world} 123} END }')
 		self.assertEqual('{hello {world} 123}', result1)
 		self.assertEqual(' END }', result2)
 
-	def test_SafeParseInt(self):
-		self.assertEqual(123, bibliography.SafeParseInt('123'))
-		self.assertEqual(None, bibliography.SafeParseInt('hello world'))
+	def test_safe_parse_int(self):
+		self.assertEqual(123, bibliography.safe_parse_int('123'))
+		self.assertEqual(None, bibliography.safe_parse_int('hello world'))
 
-	def test_NormalizeAuthor(self):
-		self.assertEqual('David Pal', bibliography.NormalizeAuthor('David Pal'))
-		self.assertEqual('David Pal', bibliography.NormalizeAuthor('Pal, David'))
+	def test_normalize_author(self):
+		self.assertEqual('David', bibliography.normalize_author('David'))
+		self.assertEqual('David', bibliography.normalize_author('  David  '))
+		self.assertEqual('David Pal', bibliography.normalize_author('  David   Pal  '))
+		self.assertEqual('David Pal', bibliography.normalize_author('Pal, David'))
+		self.assertEqual('Paul R. Halmos', bibliography.normalize_author(' Halmos ,  Paul   R. '))
+		self.assertEqual('David Pal and Francesco Orabona', bibliography.normalize_authors('Pal, David and Francesco Orabona'))
 
-	def test_NormalizeAuthor(self):
-		self.assertEqual('David Pal', bibliography.NormalizeAuthors('David Pal'))
-		self.assertEqual('David Pal and Francesco Orabona', bibliography.NormalizeAuthors('Pal, David and Francesco Orabona'))
+	def test_normalize_pages(self):
+		self.assertEqual('123--456', bibliography.normalize_pages(' 123 -- 456 '))
+		self.assertEqual('123--456', bibliography.normalize_pages('123-456'))
+		self.assertEqual('123--456', bibliography.normalize_pages('  123  -  456  '))
+		self.assertEqual('123', bibliography.normalize_pages('123'))
 
-	def test_NormalizePages(self):
-		self.assertEqual('123--456', bibliography.NormalizePages(' 123 -- 456 '))
-		self.assertEqual('123--456', bibliography.NormalizePages('123-456'))
-		self.assertEqual('123', bibliography.NormalizePages('123'))
+	def test_normalize_year(self):
+		self.assertEqual('not_a_year', bibliography.normalize_year('  not_a_year  '))
+		self.assertEqual('2019', bibliography.normalize_year('  2019  '))
+		self.assertEqual('1887', bibliography.normalize_year('1887'))
+		self.assertEqual('1995', bibliography.normalize_year('  95 '))
+		self.assertEqual('2015', bibliography.normalize_year('2015'))
 
-	def test_NormalizeYear(self):
-		self.assertEqual('1887', bibliography.NormalizeYear('1887'))
-		self.assertEqual('1995', bibliography.NormalizeYear('  95 '))
-		self.assertEqual('2015', bibliography.NormalizeYear('2015'))
-
-	def test_NormalizeMonth(self):
-		self.assertEqual('January', bibliography.NormalizeMonth('january'))
-		self.assertEqual('April', bibliography.NormalizeMonth('apr'))
-		self.assertEqual('September', bibliography.NormalizeMonth('sept'))
-		self.assertEqual('December', bibliography.NormalizeMonth('decem'))
-		self.assertEqual('unknown month', bibliography.NormalizeMonth('unknown month'))
+	def test_normalize_month(self):
+		self.assertEqual('January', bibliography.normalize_month('january'))
+		self.assertEqual('April', bibliography.normalize_month('apr'))
+		self.assertEqual('September', bibliography.normalize_month('sept'))
+		self.assertEqual('December', bibliography.normalize_month('decem'))
+		self.assertEqual('unknown month', bibliography.normalize_month('unknown month'))
 
 
 if __name__ == '__main__':
